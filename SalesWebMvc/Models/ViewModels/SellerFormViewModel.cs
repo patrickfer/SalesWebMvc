8d7b7846239fbd9ab1090c300bb59d663ee62cff
41548 <41548@NB243455>
2023-01-02 13:41:29 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
+
